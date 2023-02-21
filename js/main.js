35dbff746b0547413d4f2fb2f6c24b8b5b7968f9
Oybek <yazdonkulov2008>
2023-02-21 14:55:20 +0500

    salom

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ let a="+998 90" , b="448" , c="88" , d="48"
 console.log(a, Math.ceil(Math.random(b)*448), 
 Math.ceil(Math.random(c)*88), 
 Math.ceil(Math.random(d)*48))
+// Math.ceil(Math.random(d)*48))
+// Math.ceil(Math.random(d)*48))
+// Math.ceil(Math.random(d)*48))
